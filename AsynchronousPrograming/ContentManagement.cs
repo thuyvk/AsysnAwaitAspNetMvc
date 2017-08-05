b55c3c8078f24d623935d8bfd31e17fbcb8cc82a
Thuyvk's Blog <khanhthuy46k2dhv@gmail.com>
2017-08-05 23:55:23 +0700

    first commit

---
+++ AsynchronousPrograming/ContentManagement.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsynchronousPrograming
+{
+    public class ContentManagement
+    {
+        //synchronous method
+        public string GetContent()
+        {
+            Thread.Sleep(5000);
+            return "content";
+        }
+
+        public int GetNumber()
+        {
+            Thread.Sleep(3000);
+            return 3;
+        }
+
+        //asynchronous method
+        public async Task<string> GetContentAsync()
+        {
+            await Task.Delay(5000);
+            return "content async";
+        }
+
+        public async Task<int> GetNumbreAsync()
+        {
+            await Task.Delay(3000);
+            return 3;
+        }
+    }
+}
---
+++ AsynchronousPrograming/Controllers/DefaultController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AsynchronousPrograming.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            ContentManagement service = new ContentManagement();
+            service.GetContent();
+            service.GetNumber();
+            watch.Stop();
+            ViewBag.Milliseconds = watch.ElapsedMilliseconds;
+            return View();
+        }
+
+        public async Task<ActionResult> IndexAsync()
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            ContentManagement service = new ContentManagement();
+            var contentTask = service.GetContentAsync();
+            var numberTask = service.GetNumbreAsync();
+            await contentTask;
+            await numberTask;
+            watch.Stop();
+            ViewBag.Milliseconds = watch.ElapsedMilliseconds;
+            return View();
+        }
+    }
+}
